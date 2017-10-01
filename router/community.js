197cd37b7a243628bc3f2edfc33993894e2ac747
함나영 <saera111111@gmail.com>
2017-10-01 19:37:44 +0900

    GET /community-write uri 추가

--- router/community.js
+++ router/community.js
@@ -20,6 +20,10 @@ dbConnection.connect(function(err){
 //module.exports는 server.js에서 모듈로 불러올 수 있도록 사용됨
 module.exports = function(app, fs)
 {
+  app.get('/community/post-write', function(req, res){
+    res.render('community-write',{});
+  });
+  
 	app.post('/post-write-complete', function(req, res){
 		var result = {};
 		var json = {};
@@ -29,7 +33,7 @@ module.exports = function(app, fs)
 		console.log("***New COMMUNITY POST Request id arrived***");
 
 		//parse body
-		json = JSON.parse(req.body);		
+		json = JSON.parse(req.body);
 
 		//insert to DB
 		dbConnection.query('INSERT into Post VALUES (DEFAULT,?,?,DEFAULT,?,?,?);',[uid, json.category, json.title, json.content. json.image], function(err, result, fields){
@@ -43,7 +47,7 @@ module.exports = function(app, fs)
 				result.DATA = json;
 			}
 			res.redirect('/community');
-		});				
+		});
 	return;
 	});
 
