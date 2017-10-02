c42a53e6a123d64f5656b13d455c1700f6698793
juha <migrate1107@gmail.com>
2017-10-02 12:55:18 +0900

    캐릭터 잠금 해제 redirection

--- router/character.js
+++ router/character.js
@@ -20,7 +20,7 @@ dbConnection.connect(function(err){
 
 module.exports = function(app, fs)
 {
-	/* character */
+	/* character 첫 화면 */
   app.get('/character', function(req, res){
     console.log("***Character GET Request arrived***");
 
@@ -51,23 +51,17 @@ module.exports = function(app, fs)
          console.log(err);
       }
   	  if (data[0] != null) {
-   	     console.log(data);
-         console.log(data[0]);
-      for (var i in data ) {
-      	var n = data[i].characterType
-      	character.list[n] = 1;
-        	}
+        for (var i in data ) {
+        	var n = data[i].characterType
+        	character.list[n] = 1;
+        }
       }
 
       if(isWeb == true) {
         character.main = req.session.passport.user.mainCharacter;
-		console.log(req.session.passport.user);
-		console.log(character);
-		console.log(character.main);
-		console.log(character.list);
       	res.render('character', {
-			character: character
-		});
+    			character: character, state: req.params.state
+    		});
       } else{
         dbConnection.query('SELECT mainCharacter FROM User WHERE userId=?;',[currentUser], function(err, data){
           if(err) {
@@ -78,7 +72,42 @@ module.exports = function(app, fs)
       	});
       }
     });
-
   });
 
+  /* character unlock*/
+  app.get('/character/unlock', function(req, res){
+    console.log("***Character Unlock GET Request arrived***");
+
+    var currentUser;
+    var isWeb = false;
+
+    if((req.query.uid == undefined)){ //web
+      isWeb = true;
+      currentUser = req.session.passport.user.userId;
+    } else { //android
+      currentUser = req.query.uid;
+    }
+
+    var result = {};
+
+    dbConnection.query('SELECT characterType FROM Charact WHERE userId=?;',[currentUser], function(err, data){
+      if(err) {
+         console.log(err);
+      }
+      if (data[0] != null) {
+        // unlock 할 수 있는 상태인지
+        for (var i in data ) {
+
+        }
+        result.state = success;
+      }
+
+      if(isWeb == true) {
+        console.log("web");
+        res.redirect('/character?state'+result.state);
+      } else{
+        console.log("android");
+      }
+    });
+  });
 };
