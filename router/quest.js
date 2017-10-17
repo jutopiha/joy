0c9d678d8cd4207d66ef4d9c01d58167d12ce1e5
juha <migrate1107@gmail.com>
2017-10-17 19:57:27 +0900

    퀘스트 첫화면 수정

--- router/quest.js
+++ router/quest.js
@@ -35,24 +35,42 @@ module.exports = function(app, fs)
       currentUser = req.query.uid;
     }
 
+    var result = {};
+
     dbConnection.query('SELECT * FROM Quest WHERE userId = ?;',[currentUser], function(err, data){
       if (err) {
         console.log(err);
       }else {
-		if(data[0].type == "weekly") {
-        var endDate = parseInt(moment(data[0].startDate, 'YYYYMMDD').add(+7, 'days').format('YYYYMMDD'));
-} else if(data[0].type == "monthly") {
-var endDate = parseInt(moment(data[0].startDate, 'YYYYMMDD').add(1, 'M').format('YYYYMMDD'));
-}
-console.log(endDate);
-
-        dbConnection.query('SELECT sum(money) as money FROM Expense WHERE userId = ? AND date >= ? AND date <= ?;',[req.query.uid, startDate, endDate], function(err, data){
-          if(isWeb == true) {
-            res.redirect('/quest');
-          } else{
-            res.json("success");
+        for(var i in data) {
+          var endDate;
+          if(data[i].type == "weekly") {
+            var weekly = {};
+            weekly.type = "weekly";
+            weekly.startDate = data[i].startDate;
+            weekly.endDate = parseInt(moment(data[0].startDate, 'YYYYMMDD').add(+7, 'days').format('YYYYMMDD'));
+            weekly.goalMoney = data[i].money;
+            dbConnection.query('SELECT sum(money) as money FROM Expense WHERE userId = ? AND date >= ? AND date <= ?;',[req.query.uid, startDate, endDate], function(err, data){
+              weekly.nowMoney = data[0].money;
+            });
+            result.weekly = weekly;
+          } else if(data[i].type == "monthly") {
+            var monthly = {};
+            monthly.type = "monthly";
+            monthly.startDate = data[i].startDate;
+            monthly.endDate = parseInt(moment(data[0].startDate, 'YYYYMMDD').add(1, 'M').format('YYYYMMDD'));
+            monthly.goalMoney = data[i].money;
+            dbConnection.query('SELECT sum(money) as money FROM Expense WHERE userId = ? AND date >= ? AND date <= ?;',[req.query.uid, startDate, endDate], function(err, data){
+              monthly.nowMoney = data[0].money;
+            });
+            result.monthly = monthly;
           }
-        });
+        }
+
+        if(isWeb == true) {
+          res.redirect('/quest');
+        } else{
+          res.json(result);
+        }
       }
     });
   });
@@ -73,10 +91,8 @@ console.log(endDate);
   		currentUser = req.query.uid;
       json = JSON.parse(req.body);
   	}
+    json.startDate = parseInt(moment().format('YYYYMMDD'));
 
-
-json.startDate = parseInt(moment().format('YYYYMMDD'));
-console.log(json);
     dbConnection.query('INSERT into Quest VALUES (DEFAULT,?,?,?,?);', [json.type, currentUser, json.startDate, json.money], function (err, result, fields) {
       if (err) {
         console.log(err);
