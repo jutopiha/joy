0e00bcb1b624efc9e7bc327fc51e2bc3e3ff3228
juha <migrate1107@gmail.com>
2017-11-11 17:36:44 +0900

    legnth -> length 오타 수정

--- router/statistic.js
+++ router/statistic.js
@@ -236,14 +236,14 @@ module.exports = function(app, fs)
               }
             }
 
-            if(food.legnth > 0) statistic.whole["식비"] = ss.mean(food);
-            if(fare.legnth > 0) statistic.whole["교통비"] = ss.mean(fare);
-            if(culture.legnth > 0) statistic.whole["문화"] = ss.mean(culture);
-            if(living.legnth > 0) statistic.whole["생활"] = ss.mean(living);
-            if(snack.legnth > 0) statistic.whole["음료/간식"] = ss.mean(snack);
-            if(edu.legnth > 0) statistic.whole["교육"] = ss.mean(edu);
-            if(utility.legnth > 0) statistic.whole["공과금"] = ss.mean(utility);
-            if(etc.legnth > 0) statistic.whole["기타"] = ss.mean(etc);
+            if(food.length > 0) statistic.whole["식비"] = ss.mean(food);
+            if(fare.length > 0) statistic.whole["교통비"] = ss.mean(fare);
+            if(culture.length > 0) statistic.whole["문화"] = ss.mean(culture);
+            if(living.length > 0) statistic.whole["생활"] = ss.mean(living);
+            if(snack.length > 0) statistic.whole["음료/간식"] = ss.mean(snack);
+            if(edu.length > 0) statistic.whole["교육"] = ss.mean(edu);
+            if(utility.length > 0) statistic.whole["공과금"] = ss.mean(utility);
+            if(etc.length > 0) statistic.whole["기타"] = ss.mean(etc);
 
       			if(isWeb == true) {
       				res.render('monthlyStat', {
